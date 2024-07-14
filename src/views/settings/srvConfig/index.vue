9d575e01535ee41851c6d82febb6540461286e71
Tht <tht@sina.com>
2024-07-14 23:06:22 +0800

    重构了服务配置，增加了采集配置项。

--- src/views/settings/srvConfig/index.vue
+++ src/views/settings/srvConfig/index.vue
@@ -2,7 +2,10 @@
   <el-card class="box-card">
     <template #header>
       <div class="card-header">
-        <span>服务配置</span>
+        <el-space><span>服务配置</span></el-space>
+        <el-space>
+          <el-button @click="restartGw">重启网关服务</el-button>
+        </el-space>
       </div>
     </template>
     <el-row :gutter="20">
@@ -10,17 +13,12 @@
         <el-card class="box-card">
           <template #header>
             <div class="card-header">
-              <el-space>
-                <span>服务器基本配置</span>
-              </el-space>
-              <el-space>
-                <el-button @click="restartGw">重启网关服务</el-button>
-              </el-space>
+              <span>服务器基本配置</span>
             </div>
           </template>
           <div style="width: 100%; height: 210px; overflow: auto">
             <el-row type="flex" align="middle" style="margin-bottom: 10px">
-              <el-col :span="5" style="text-align: right"><span>服务器时间：</span></el-col>
+              <el-col :span="5" class="text-right"><span>服务器时间：</span></el-col>
               <el-col :span="14" style="min-height: 36px">
                 <el-input type="input" v-model="sysTime" style="width: 240px"></el-input>
               </el-col>
@@ -30,64 +28,23 @@
               </el-col>
             </el-row>
             <el-row type="flex" align="middle" style="margin-bottom: 10px">
-              <el-col :span="5" style="text-align: right"><span>以太网Mac地址：</span></el-col>
+              <el-col :span="5" class="text-right"><span>以太网Mac地址：</span></el-col>
               <el-col :span="14"><el-input type="input" v-model="ethMac" style="width: 240px"></el-input></el-col>
               <el-col :span="5">
                 <el-button @click="refreshEthMac">刷新</el-button>
                 <el-button @click="saveEthMac">保存</el-button>
               </el-col>
             </el-row>
-
             <el-row type="flex" align="middle" style="margin-bottom: 10px">
-              <el-col :span="5" style="text-align: right"><span>服务及日志配置：</span></el-col>
+              <el-col :span="5" class="text-right"><span>天津工场测试模式：</span></el-col>
               <el-col :span="14">
-                <el-space>
-                  <el-checkbox v-model="logShowConf.reply_original_pkg">
-                    <span>转发原始位置报文</span>
-                  </el-checkbox>
-                  <el-checkbox v-model="logShowConf.proc_ins_pkg">
-                    <span>处理INS报文</span>
-                  </el-checkbox>
-                  <el-checkbox v-model="logShowConf.reply_not_loc_pkg">
-                    <span>转发INS非位置报文</span>
-                  </el-checkbox>
-                </el-space>
-                <el-space>
-                  <el-checkbox v-model="logShowConf.sync_sys_time_by_cpt7">
-                    <span>同步CPT7时间</span>
-                  </el-checkbox>
-                  <el-checkbox v-model="logShowConf.sync_sys_time_by_ins">
-                    <span>同步INS时间</span>
-                  </el-checkbox>
-                  <span>采集等待:</span>
-                  <el-input-number
-                    v-model="logShowConf.timeout_ms"
-                    controls-position="right"
-                    :min="0"
-                    :max="990"
-                    :step="10"
-                    style="width: 5em"
-                  ></el-input-number>
-                  <span>ms</span>
-                </el-space>
-                <el-space>
-                  <el-checkbox v-model="logShowConf.show_filter_pkg">
-                    <span>UDP包过滤</span>
-                  </el-checkbox>
-                  <el-checkbox v-model="logShowConf.show_loc_info">
-                    <span>坐标数据</span>
-                  </el-checkbox>
-                  <el-checkbox v-model="logShowConf.show_pkg_info">
-                    <span>UDP包摘要</span>
-                  </el-checkbox>
-                  <el-checkbox v-model="logShowConf.show_sec_info">
-                    <span>偏转任务</span>
-                  </el-checkbox>
-                </el-space>
+                <el-checkbox v-model="tjTestMode">
+                  <span class="stdfs">{{ tjTestMode ? "打开" : "关闭" }}</span>
+                </el-checkbox>
               </el-col>
               <el-col :span="5">
-                <el-button @click="loadLogShowConf">刷新</el-button>
-                <el-button @click="saveLogShowConf">保存</el-button>
+                <el-button @click="loadTJTestMode">刷新</el-button>
+                <el-button @click="saveTJTestMode">保存</el-button>
               </el-col>
             </el-row>
           </div>
@@ -103,28 +60,15 @@
           <div style="width: 100%; height: 210px; overflow: auto">
             <el-row type="flex" align="middle">
               <el-col :span="24">
-                <el-input type="textarea" v-model="ethConf" :rows="4"></el-input>
+                <el-input type="textarea" v-model="ethConf" :rows="6"></el-input>
               </el-col>
             </el-row>
-            <el-row type="flex" justify="center" align="middle">
-              <el-col :span="24">
+            <el-row type="flex" align="middle">
+              <el-col :span="24" class="text-right">
                 <el-button style="margin-right: 10px" @click="refreshEthConf">刷新</el-button>
                 <el-button style="margin-right: 10px" @click="getEthConfDefault">加载默认配置</el-button>
                 <el-button @click="saveEthConf">保存</el-button>
                 <el-button @click="rebootDevCall">重启设备</el-button>
-              </el-col>
-            </el-row>
-            <el-row type="flex" align="middle" style="margin-bottom: 10px">
-              <el-col :span="5" style="text-align: right"><span>天津工场测试模式：</span></el-col>
-              <el-col :span="5">
-                <el-checkbox v-model="tjTestMode">
-                  <span>{{ tjTestMode ? "打开" : "关闭" }}</span>
-                </el-checkbox>
-              </el-col>
-              <el-col :span="9">
-                <el-button @click="loadTJTestMode">刷新</el-button>
-                <el-button @click="saveTJTestMode">保存</el-button>
-                <el-button @click="restartGw">重启网关服务</el-button>
               </el-col>
             </el-row>
           </div>
@@ -136,46 +80,149 @@
         <el-card class="box-card">
           <template #header>
             <div class="card-header">
-              <span>INS数据处理配置(保存后重启网关服务方可生效)</span>
+              <span>采集服务及日志配置(保存后重启网关服务方可生效)</span>
             </div>
           </template>
-          <div style="width: 100%; height: 296px; overflow: auto">
-            <el-row type="flex" align="middle">
-              <el-col :span="24">
-                <el-input type="textarea" v-model="insConf" :rows="10"></el-input>
-              </el-col>
-            </el-row>
-            <el-row type="flex" justify="center" align="middle">
-              <el-col :span="24">
-                <el-button style="margin-right: 10px" @click="refreshInsConf">刷新</el-button>
-                <el-button style="margin-right: 10px" @click="getInsConfDefault">加载默认配置</el-button>
-                <el-button @click="saveInsConf">保存</el-button>
-              </el-col>
-            </el-row>
-          </div>
+          <el-row type="flex" align="middle" style="margin-bottom: 10px">
+            <el-col :span="5" class="text-right">采集数据配置：</el-col>
+            <el-col :span="19">
+              <div>
+                <el-space>
+                  <span>采集网卡:</span>
+                  <el-input type="input" v-model="logShowConf.cap_device" style="width: 70px"></el-input>
+                  <span>采集等待:</span>
+                  <el-input-number
+                    v-model="logShowConf.timeout_ms"
+                    controls-position="right"
+                    :min="0"
+                    :max="2000"
+                    :step="100"
+                    style="width: 6em"
+                  ></el-input-number>
+                  <span class="stdfs">毫秒</span>
+                  <el-checkbox v-model="logShowConf.promisc_mode">
+                    <span class="stdfs">混杂模式</span>
+                  </el-checkbox>
+                </el-space>
+              </div>
+              <div>
+                <el-space>
+                  <span class="stdfs">INS采集端口:</span>
+                  <el-input-number
+                    v-model="logShowConf.ins_cap_port"
+                    controls-position="right"
+                    :min="1025"
+                    :max="65534"
+                    :step="100"
+                    style="width: 7em"
+                  ></el-input-number>
+                </el-space>
+                <el-space>
+                  <span class="stdfs">CPT7采集端口:</span>
+                  <el-input-number
+                    v-model="logShowConf.cpt7_cap_port"
+                    controls-position="right"
+                    :min="1025"
+                    :max="65534"
+                    :step="100"
+                    style="width: 7em"
+                  ></el-input-number>
+                </el-space>
+              </div>
+              <div>
+                <el-space>
+                  <span>采集过滤条件:</span>
+                  <el-input type="input" v-model="logShowConf.filter_info" style="width: 420px"></el-input>
+                </el-space>
+              </div>
+            </el-col>
+            <el-col :span="5" class="text-right">系统服务配置：</el-col>
+            <el-col :span="19">
+              <el-space>
+                <el-checkbox v-model="logShowConf.reply_original_pkg">
+                  <span class="stdfs">转发原始位置报文</span>
+                </el-checkbox>
+                <el-checkbox v-model="logShowConf.proc_ins_pkg">
+                  <span class="stdfs">处理INS报文</span>
+                </el-checkbox>
+                <el-checkbox v-model="logShowConf.reply_not_loc_pkg">
+                  <span class="stdfs">转发INS非位置报文</span>
+                </el-checkbox>
+              </el-space>
+              <el-space>
+                <el-checkbox v-model="logShowConf.sync_sys_time_by_cpt7">
+                  <span class="stdfs">同步CPT7时间</span>
+                </el-checkbox>
+                <el-checkbox v-model="logShowConf.sync_sys_time_by_ins">
+                  <span class="stdfs">同步INS时间</span>
+                </el-checkbox>
+              </el-space>
+            </el-col>
+            <el-col :span="5" class="text-right">日志输出配置：</el-col>
+            <el-col :span="19">
+              <el-space>
+                <el-checkbox v-model="logShowConf.show_filter_pkg">
+                  <span class="stdfs">UDP包过滤</span>
+                </el-checkbox>
+                <el-checkbox v-model="logShowConf.show_loc_info">
+                  <span class="stdfs">坐标数据</span>
+                </el-checkbox>
+                <el-checkbox v-model="logShowConf.show_pkg_info">
+                  <span class="stdfs">UDP包摘要</span>
+                </el-checkbox>
+                <el-checkbox v-model="logShowConf.show_sec_info">
+                  <span class="stdfs">偏转任务</span>
+                </el-checkbox>
+              </el-space>
+            </el-col>
+          </el-row>
+          <el-row type="flex" align="middle">
+            <el-col :span="24" class="text-right">
+              <el-button @click="loadLogShowConf">刷新</el-button>
+              <el-button @click="saveLogShowConf">保存</el-button>
+            </el-col>
+          </el-row>
         </el-card>
       </el-col>
       <el-col :span="12">
         <el-card class="box-card">
           <template #header>
             <div class="card-header">
-              <span>CPT7数据处理配置(保存后重启网关服务方可生效)</span>
+              <span>数据处理配置(保存后重启网关服务方可生效)</span>
             </div>
           </template>
-          <div style="width: 100%; height: 296px; overflow: auto">
-            <el-row type="flex" align="middle">
-              <el-col :span="24">
-                <el-input type="textarea" v-model="cpt7Conf" :rows="10"></el-input>
-              </el-col>
-            </el-row>
-            <el-row type="flex" justify="center" align="middle">
-              <el-col :span="24">
-                <el-button style="margin-right: 10px" @click="refreshCpt7Conf">刷新</el-button>
-                <el-button style="margin-right: 10px" @click="getCpt7ConfDefault">加载默认配置</el-button>
-                <el-button @click="saveCpt7Conf">保存</el-button>
-              </el-col>
-            </el-row>
-          </div>
+          <el-tabs type="card" v-model="activeTab">
+            <el-tab-pane label="CPT7数据处理配置" name="cpt7">
+              <!-- CPT7配置内容 -->
+              <el-row type="flex" align="middle">
+                <el-col :span="24">
+                  <el-input type="textarea" v-model="cpt7Conf" :rows="7"></el-input>
+                </el-col>
+              </el-row>
+              <el-row type="flex" align="middle">
+                <el-col :span="24" class="text-right">
+                  <el-button style="margin-right: 10px" @click="refreshCpt7Conf">刷新</el-button>
+                  <el-button style="margin-right: 10px" @click="getCpt7ConfDefault">加载默认配置</el-button>
+                  <el-button @click="saveCpt7Conf">保存</el-button>
+                </el-col>
+              </el-row>
+            </el-tab-pane>
+            <el-tab-pane label="INS数据处理配置" name="ins">
+              <!-- INS配置内容 -->
+              <el-row type="flex" align="middle">
+                <el-col :span="24">
+                  <el-input type="textarea" v-model="insConf" :rows="7"></el-input>
+                </el-col>
+              </el-row>
+              <el-row type="flex" align="middle">
+                <el-col :span="24" class="text-right">
+                  <el-button style="margin-right: 10px" @click="refreshInsConf">刷新</el-button>
+                  <el-button style="margin-right: 10px" @click="getInsConfDefault">加载默认配置</el-button>
+                  <el-button @click="saveInsConf">保存</el-button>
+                </el-col>
+              </el-row>
+            </el-tab-pane>
+          </el-tabs>
         </el-card>
       </el-col>
     </el-row>
@@ -208,8 +255,7 @@ import {
 } from "@/api/modules/sysadmin";
 // 导入共用方法
 import { confirmAction } from "@/api/modules/utilfuns";
-// import { ElMessage } from 'element-plus';
-// import { getSysTime, setSysTime } from '@/api/settings/srvConfig';
+const activeTab = ref("cpt7");
 const sysTime = ref();
 const ethMac = ref();
 const ethConf = ref();
@@ -226,6 +272,11 @@ const logShowConf = ref({
   sync_sys_time_by_cpt7: true,
   sync_sys_time_by_ins: false,
   proc_ins_pkg: false,
+  promisc_mode: false,
+  ins_cap_port: 30018,
+  cpt7_cap_port: 7003,
+  filter_info: "udp",
+  cap_device: "eth0",
   timeout_ms: 3
 });
 
