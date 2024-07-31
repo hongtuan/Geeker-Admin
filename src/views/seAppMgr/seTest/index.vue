58b3ea615b7e753a73b71d85f71d810962d87605
Tht <tht@sina.com>
2024-07-31 13:42:48 +0800

    偏转功能测试界面和测试结果中增加了GPSWeek项

--- src/views/seAppMgr/seTest/index.vue
+++ src/views/seAppMgr/seTest/index.vue
@@ -1,69 +1,13 @@
-<!-- <template>
-  <el-card class="box-card">
-    <template #header>
-      <div class="card-header">
-        <span>偏转芯片测试</span>
-      </div>
-    </template>
-    <el-row>
-      <el-col>
-        <el-form :model="formData" label-width="140px">
-          <el-form-item label="经度lng:">
-            <el-input v-model="formData.lng" class="input-line" />
-          </el-form-item>
-          <el-form-item label="纬度lat:">
-            <el-input v-model="formData.lat" class="input-line" />
-          </el-form-item>
-          <el-form-item label="高度heit:">
-            <el-input v-model="formData.heit" class="input-line" />
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="onSubmit"> 确定 </el-button>
-            <el-button>重置</el-button>
-          </el-form-item>
-        </el-form>
-        <el-input v-model="testResult" type="textarea" :rows="10" style="width: 640px" />
-      </el-col>
-    </el-row>
-  </el-card>
-</template>
-
-<script setup lang="ts" name="seTest">
-// import dayjs from "dayjs";
-import { seTestSrv } from "@/api/modules/seappmgr";
-import { ElMessage } from "element-plus";
-import { ref, reactive } from "vue";
-
-// do not use same name with ref
-const formData = reactive({
-  lng: "125.15588895",
-  lat: "44.33206306",
-  heit: "200.6"
-});
-
-const testResult = ref("");
-
-const onSubmit = async () => {
-  // ElMessage.success("提交的数据为 : " + JSON.stringify(formData));
-  const { data } = await seTestSrv(formData);
-  console.log(data);
-  testResult.value = JSON.stringify(data, null, 2);
-  ElMessage.success("测试完成，请查看响应数据。");
-};
-</script>
-
-<style scoped lang="scss">
-@import "./index.scss";
-</style> -->
 <template>
   <el-card class="box-card">
     <template #header>
       <div class="card-header">
-        <span>偏转芯片测试</span>
+        <span>偏转功能测试</span>
       </div>
     </template>
     <el-row>
       <el-col>
+        <div><b>测试输入</b></div>
         <el-form :model="formData" :rules="rules" label-width="140px" ref="formRef">
           <el-form-item label="经度lng:" prop="lng">
             <el-input v-model="formData.lng" class="input-line" />
@@ -74,12 +18,16 @@ const onSubmit = async () => {
           <el-form-item label="高度heit:" prop="heit">
             <el-input v-model="formData.heit" class="input-line" />
           </el-form-item>
+          <el-form-item label="GPSWeek:" prop="gpsWeek">
+            <el-input v-model="formData.gpsWeek" class="input-line" />
+          </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">确定</el-button>
+            <el-button type="primary" @click="onSubmit">测试</el-button>
             <el-button @click="onReset">重置</el-button>
           </el-form-item>
         </el-form>
-        <el-input v-model="testResult" type="textarea" :rows="10" style="width: 640px" />
+        <div><b>测试输出</b></div>
+        <el-input v-model="testResult" type="textarea" :rows="15" style="width: 520px" />
       </el-col>
     </el-row>
   </el-card>
@@ -94,7 +42,8 @@ const formRef = ref<typeof ElForm | null>(null);
 const formData = reactive({
   lng: "125.15588895",
   lat: "44.33206306",
-  heit: "200.6"
+  heit: "200.6",
+  gpsWeek: "2306"
 });
 
 const testResult = ref("");
@@ -111,6 +60,10 @@ const rules = {
   heit: [
     { required: true, message: "高度不能为空", trigger: "blur" },
     { pattern: /^\-?\d+(\.\d+)?$/, message: "高度必须是数字", trigger: "blur" }
+  ],
+  gpsWeek: [
+    { required: true, message: "gpsWeek不能为空", trigger: "blur" },
+    { pattern: /^\-?\d+(\.\d+)?$/, message: "gpsWeek必须是数字", trigger: "blur" }
   ]
 };
 
